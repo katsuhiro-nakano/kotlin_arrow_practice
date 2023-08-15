3627ec0f2a93ef18e930aacc27b939c0bb8ad254
knakano <katsuhiro_nakano@trista.jp>
2023-08-15 12:55:32 +0900

    arrowを導入

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,8 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+    implementation("io.arrow-kt:arrow-core:1.2.0")
+    implementation("io.arrow-kt:arrow-fx-coroutines:1.2.0")
 }
 
 tasks.test {
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,16 @@
+import arrow.core.Either
+import arrow.core.raise.either
+
 fun main(args: Array<String>) {
     println("Hello World!")
 
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
     println("Program arguments: ${args.joinToString()}")
+
+    val either:Either<String, Int> = Either.Left("this is left")
+    val either2:Either<String, Int> = Either.Right(10000)
+
+    println(either)
+    println(either2)
 }
